93b8a51df06ac56439bb58ca22d506b9ed0c44fe
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-02-16 16:35:04 -0300

    implemented aditional functionality

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 // operations
 function add(firstNumber, secondNumber) {
-    console.log(+firstNumber);
-    return +firstNumber + +secondNumber;
+    let sum =  +firstNumber + +secondNumber;
+    return(roundNumber(sum));
 }
 
 function substract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
+    let subs = firstNumber - secondNumber;
+    return(roundNumber(subs));
 }
 
 function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
+    let product = firstNumber * secondNumber;
+    return(roundNumber(+product));
 }
 
 function divide(firstNumber, secondNumber) {
-    return firstNumber / secondNumber;
+    if ( secondNumber == 0 ) {
+        clearDisplay();
+        alert("CAN'T DIVIDE BY 0!!");
+        return;
+    }
+    let result = firstNumber / secondNumber;
+    return(roundNumber(+result));
 }
 // end operations
 
@@ -28,16 +36,21 @@ buttons.forEach(button => button.addEventListener('click', function (button) {
 }))
 
 const inputDisplay = document.querySelector('#input');
+const decimalButton = document.querySelector('#decimal');
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 
 function display ( value ) {
-    if ( value == +value ) {
+    if ( value == +value || value == "." ) {
+        if ( value == "." ) {
+            decimalButton.disabled = true;
+        }
         if ( firstNumber == "") {
             inputDisplay.textContent += value;
         } else if ( secondNumber == "" ) {
             inputDisplay.textContent = "";
+            decimalButton.disabled = false;
             inputDisplay.textContent += value;
             secondNumber = inputDisplay.textContent;
         } else {
@@ -46,6 +59,8 @@ function display ( value ) {
         }
     } else if ( value == "clear" ) {
         clearDisplay();
+    } else if ( value == "backspace" ){
+        inputDisplay.textContent = inputDisplay.textContent.slice(0,-1);
     } else {
         compute(value);
     }
@@ -64,7 +79,11 @@ function compute ( value ) {
         }
     } else {
         if ( firstNumber != "" && secondNumber != "" ) {
+            secondNumber = inputDisplay.textContent;
             inputDisplay.textContent = operate(firstNumber, secondNumber, operator);
+        } else { 
+            clearDisplay();
+            alert("MISSING PARAMETERS");
         }
     }
 }
@@ -74,4 +93,34 @@ function clearDisplay () {
     firstNumber = "";
     secondNumber = "";
     operator = "";
+    decimalButton.disabled = false;
 }
+
+
+
+//rounding number function.
+function roundNumber(num) {
+    return Math.round((num + Number.EPSILON) * 100) / 100
+}
+
+
+//keyboard support
+
+calculator = document.querySelector('.container');
+
+function displayKey ( event ) {
+
+    let keyPressed = event.key;
+    console.log(typeof(keyPressed));
+
+    let button = Array.from(buttons).find(button => button.textContent === keyPressed);
+    console.log(typeof(button));
+
+    if ( typeof(button) != 'undefined' ) {
+        display(button.value);
+    }
+}
+
+document.addEventListener('keydown', displayKey);
+
+
